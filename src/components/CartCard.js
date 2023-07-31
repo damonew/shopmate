68941a641f94eb34dd15261022d84a9f30a7155c
Damon Williams <damonwilliams246@gmail.com>
2023-07-30 22:47:45 -0500

    Lesson completed commit before final cleanup and deployment.

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,18 +1,19 @@
+import { useCart } from "../context/CartContext";
 import { useTitle } from "../hooks/useTitle";
 
 import "./CartCard.css";
 
 export const CartCard = ({ product }) => {
   useTitle('Cart');
-
-  const {name, price, image} = product;
-
+  const { removeFromCart} = useCart();
+  const { name, price, image } = product;
+  
   return (
       <div className="cartCard">
         <img src={image} alt={name} />
         <p className="productName">{name}</p>
         <p className="productPrice">${price}</p>
-        <button>Remove</button>
+      <button onClick={() => removeFromCart(product)}>Remove</button>
       </div>
   )
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
+import { useCart } from "../context/CartContext";
+
 import "./ProductCard.css";
 
-export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
+export const ProductCard = ({ product }) => {
+  const { cartList, addToCart, removeFromCart } = useCart();
+  const [isInCart, setIsInCart] = useState(false);
+  const { id, name, price, image } = product;
+
+  useEffect(() => {
+    setIsInCart(cartList.find(cartItem => cartItem.id === id));
+  }, [cartList, id])
 
   return (
     <div className="productCard">
@@ -9,7 +18,11 @@ export const ProductCard = ({product}) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button>Add To Cart</button>
+        {isInCart ?
+          <button className="remove" onClick={() => removeFromCart(product)}>Remove</button>
+          :
+          <button className="add" onClick={() => addToCart(product)}>Add To Cart</button>
+        }
       </div>
     </div>
   )
